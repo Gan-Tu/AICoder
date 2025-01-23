5611dfd60dbd65be4bcceb01533b921529711ed0
Gan Tu <dev@tugan.io>
2025-01-24 01:15:16 +0800

    support install pkgs

--- src/app/api/execute-script/route.ts
+++ src/app/api/execute-script/route.ts
@@ -1,6 +1,8 @@
 import { Sandbox } from '@e2b/code-interpreter';
 import { NextRequest, NextResponse } from 'next/server';
 import OpenAI from 'openai';
+import { zodResponseFormat } from "openai/helpers/zod";
+import { z } from "zod";
 
 // Allow streaming responses up to 60 seconds
 export const maxDuration = 60;
@@ -13,38 +15,46 @@ export async function POST(request: NextRequest) {
   try {
     const { userPrompt } = await request.json();
 
-    const completion = await openai.chat.completions.create({
+    const completion = await openai.beta.chat.completions.parse({
       model: "gpt-4o-mini",
       messages: [
-        { role: "system", content: "You are a helpful assistant that can execute python code in a Jupyter notebook. Only respond with the code to be executed and nothing else. Strip backticks in code blocks. Do not use packages that don't come with python by default" },
+        { role: "system", content: "You are a helpful assistant that can execute python code in a Jupyter notebook. Only respond with the code to be executed and nothing else. Strip backticks in code blocks. Prefer using packages that come with python by default. If new python packages are needed, list each package name in the `packages` output format that can be run individually using pip install. Leave `packages` output field empty if no new packages are needed." },
         {
           role: "user",
           content: userPrompt,
         },
       ],
+      response_format: zodResponseFormat(z.object({
+        packages: z.array(z.string()).describe("A list of python packages to install using pip install. Leave empty if no new packages are needed."),
+        code: z.string().describe("The code snippet to execute in python shell."),
+      }), "script"),
     });
 
-    const generatedCode = completion.choices[0].message.content;
 
-    if (!generatedCode) {
+    const script = completion.choices[0].message.parsed;
+    if (!script || !script.code) {
       return NextResponse.json(
-        { success: false, error: 'Failed to generate code.' },
+        { success: false, error: 'Failed to generate script.' },
         { status: 404 }
       );
     }
 
-
     const sandbox = await Sandbox.create()
-    const execution = await sandbox.runCode(generatedCode, { language: "python" })
+    if (script.packages) {
+      for (const pkg of script.packages) {
+        sandbox.commands.run(`pip install ${pkg}`)
+      }
+    }
+    const execution = await sandbox.runCode(script.code, { language: "python" })
     await sandbox.kill();
 
     return NextResponse.json({
       success: true,
-      generatedCode,
+      generatedCode: script.code,
       output: execution.text || execution.logs.stdout.join('\n') || execution.logs.stderr.join('\n'),
     });
   } catch (error) {
-    console.error('Error executing script:', error);
+    console.error(error)
     return NextResponse.json(
       { success: false, error: 'Failed to execute script.' },
       { status: 500 }
